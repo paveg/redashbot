6cfc41e607cef7210dd6eb81727862e9173c42a7
Ryota Ikezawa <ikezawa-ryota@c-fo.com>
2018-08-17 19:57:56 +0900

    Fix up

---
+++ main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestIsTextMatch(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		subject  string
+		argument string
+		result   bool
+	}{
+		{
+			subject:  "matched argument",
+			argument: "https://test/queries",
+			result:   true,
+		},
+		{
+			subject:  "does not match argument",
+			argument: "https://www.google.co.jp",
+			result:   false,
+		},
+	}
+
+	for _, te := range tests {
+		t.Run(te.subject, func(t *testing.T) {
+			if isTextMatch(te.argument) != te.result {
+				t.Errorf("returns value is invalid[argument: %v, result: %v]\n", te.argument, te.result)
+			}
+		})
+	}
+}
